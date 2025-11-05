22f9f7fc4655f3912833743305728c4e009c7d7b
Sureshtr1998 <suresh@stepchange.earth>
2025-11-05 18:08:42 +0530

    Contact us and pricing info

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,27 +1,63 @@
+import { useState, useEffect } from "react";
 import { PRICING_COST, SUPPORT_EMAIL } from "../../utils/constants";
 import "./Info.scss";
 
 const Info = () => {
+  const [showMail, setShowMail] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowMail((prev) => !prev);
+    }, 2000); // toggle every 2 seconds
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="interactive-element-container ml-8">
       <div className="relative inline-block icon-group group">
-        <div className="click-me-badge">Click Me!</div>
+        {/* <div className="click-me-badge">Click Me!</div> */}
 
         <div
           className="heart-icon-wrapper"
           tabIndex={0}
           aria-label="View Subscription and Contact Information">
+          {/* ❤️ Heart SVG as Background */}
           <svg
+            className="heart-base"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="currentColor">
-            <path d="M11.645 20.917c-.156.095-.328.167-.507.218-.216.06-.445.06-.667 0-.179-.051-.351-.123-.507-.218-2.618-1.554-7.699-5.118-8.73-8.816C.767 7.054 2.722 4 5.513 4c1.516 0 2.879.626 3.982 1.637l1.326 1.258 1.326-1.258c1.103-1.011 2.466-1.637 3.982-1.637 2.791 0 4.746 3.054 3.73 6.601-1.03 3.698-6.112 7.262-8.73 8.816Z" />
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <path d="M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.6l-1-1a5.5 5.5 0 0 0-7.8 7.8l1 1L12 21l7.8-7.6 1-1a5.5 5.5 0 0 0 0-7.8z" />
           </svg>
+
+          {/* 💌 Mail Icon */}
+          <div className={`inner-icon icon-fade ${showMail ? "show" : "hide"}`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.63A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.12.81.38 1.61.73 2.37a2 2 0 0 1-.45 2.11L8.09 9.91a16.06 16.06 0 0 0 6 6l1.71-1.71a2 2 0 0 1 2.11-.45c.76.35 1.56.61 2.37.73a2 2 0 0 1 1.72 2z" />
+            </svg>
+          </div>
+
+          {/* ₹ Rupee Icon */}
+          <div className={`inner-icon icon-fade ${showMail ? "hide" : "show"}`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path d="M5 4h14v2H9.55a4.5 4.5 0 0 1 4.4 3.5H19v2h-4.96a4.5 4.5 0 0 1-4.4 3.5H11l5 6h-2.8l-5-6H5v-2h4.8a2.5 2.5 0 0 0 2.45-2H5V9h7.25A2.5 2.5 0 0 0 9.8 7H5V4z" />
+            </svg>
+          </div>
         </div>
 
         <div className="tooltip-popover">
           <p className="title">Pricing Details:</p>
-          <p className="body"> ₹ {PRICING_COST}/year</p>
+          <p className="body">₹ {PRICING_COST}/year</p>
 
           <hr className="border-gray-200 mb-4" />
 
