3bc29c2d9b44e5122f95f285cf58d1c344190dc6
Sureshtr1998 <suresh@stepchange.earth>
2026-01-12 12:02:05 +0530

    Total records admin

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -9,7 +9,7 @@ import ganesh from "../../assets/ganesh.webp";
 import { useToast } from "../../components/toastProvider/ToastProvider";
 import Spinner from "../../components/spinner/Spinner";
 import { remainingInterest } from "../../utils/utils";
-import { isBroker } from "../../utils/constants";
+import { isAdmin, isBroker } from "../../utils/constants";
 
 interface Props {
   filterData: UserDetails;
@@ -77,6 +77,13 @@ const Home = (props: Props) => {
     <div className="home-cards">
       <Spinner isLoading={isLoading} />
       <div>
+        {isAdmin && (
+          <p className="text-center text-2xl text-orange-400">
+            {" "}
+            {totalRecords}
+          </p>
+        )}
+
         <div className="profile-cards">
           {matches.length > 0 ? (
             matches.map((match) => (
